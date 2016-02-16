dcc2e7fd402707fc8102a841f736e60b05afbbc0
Daniel McEvoy <dmcevoy1@asu.edu>
2016-02-15 22:54:11 -0700

    Automatically Refresh Agenda Panel/JUnit also added
    The Agenda Tab now automatically refreshes.  A test package and
    TestAgendaRefresh have been created, but filled in.  Also, the JUnit 4
    jar has been added to the class path of the build.xml.

---
+++ src/net/sf/memoranda/test/TestAgendaRefresh.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package net.sf.memoranda.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Daniel McEvoy
+ *
+ */
+public class TestAgendaRefresh {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testNewProjectRefresh() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteProjectRefresh() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNewTaskRefresh() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDateChangeRefresh() {
+		fail("Not yet implemented");
+	}
+}
