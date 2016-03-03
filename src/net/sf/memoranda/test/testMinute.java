f89a59caacbc7cdd0f1bfa1665a9e6220240611f
mmehrotr <mmehrotr@asu.edu>
2016-03-02 20:07:39 -0700

    Created unit testing class

---
+++ src/net/sf/memoranda/test/testMinute.java
@@ -0,0 +1,48 @@
+package net.sf.memoranda.test;
+
+
+import static org.junit.Assert.*;
+import net.sf.memoranda.EventsManager;
+import net.sf.memoranda.EventsScheduler;
+import net.sf.memoranda.date.CalendarDate;
+import net.sf.memoranda.ui.App;
+import net.sf.memoranda.ui.EventDialog;
+import net.sf.memoranda.util.Local;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testMinute {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEventMinute() {
+		int repeatType=5;
+		EventDialog dialog =new EventDialog(App.getFrame(), Local.getString("New event"));
+		CalendarDate startDate=CalendarDate.today();
+		CalendarDate endDate= CalendarDate.tomorrow();
+		int period=(Integer) dialog.dayOfMonthSpin.getModel().getValue();
+		int hour=5;
+		int minute=12;
+		String text="hello";
+		boolean workDays= dialog.workingDaysOnlyCB.isSelected();
+		EventsManager.createRepeatableEvent(repeatType, startDate, endDate, period, hour, minute, text, workDays);
+		assertNotNull(EventsScheduler.getScheduledEvents());
+		
+	}
+
+}
