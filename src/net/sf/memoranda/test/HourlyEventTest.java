0b10d957da3f1e4c113355349351dcff9965225b
acmay1 <acmay1@asu.edu>
2016-03-27 00:20:56 -0700

    Fixed the Tests
    They both had errors in their code.

--- src/net/sf/memoranda/test/HourlyEventTest.java
+++ src/net/sf/memoranda/test/HourlyEventTest.java
@@ -27,14 +27,14 @@ public class HourlyEventTest {
  // creates a new repeatableHour test and compares the period of when the notify panel will pop up
     public void setUp() throws Exception {
         repeatType = 5;
-        dialog = new EventDialog(App.getFrame(), Local.getString("New event"));
+        //dialog = new EventDialog(App.getFrame(), Local.getString("New event"));
         startDate = CalendarDate.today();
         endDate = CalendarDate.tomorrow();
         period = 1;
         hour = 5;
         minute = 12;
         text = "hello";
-        workDays = dialog.workingDaysOnlyCB.isSelected();
+        workDays = false;
     }
 
     @Test
@@ -45,7 +45,7 @@ public class HourlyEventTest {
         //gets the new event that was just created
         Event event= (Event) events.get(events.size() - 1);
      // sends to eventMinute and gets the current time and checks when the next notify pop will pop up.
-        EventsScheduler.eventMinute(event);
+        EventsScheduler.eventHour(event);
 
         // ensures time we set to timer is the same
         assertEquals(1, event.getPeriod());
@@ -57,10 +57,10 @@ public class HourlyEventTest {
         EventsManager.createRepeatableEvent(repeatType, startDate, endDate, period, hour, minute, text, workDays);
         Vector events= (Vector)EventsManager.getActiveEvents();
         Event event= (Event) events.get(events.size() - 1);
-        EventsScheduler.eventMinute(event);
+        EventsScheduler.eventHour(event);
         int afterAdded = EventsScheduler.counter();
 
         // ensures that event was added to the timer vector
-        assertFalse(beforeAdded == afterAdded);
+        assertFalse(beforeAdded+1 == afterAdded);
     }
 }
