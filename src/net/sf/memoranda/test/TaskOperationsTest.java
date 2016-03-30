4757d0f2fdb8c828b81b9ef42486c02cef596955
Daniel McEvoy <dmcevoy1@asu.edu>
2016-03-29 23:38:22 -0700

    Updated Unit test. Known issues persist with recurring tasks
    2 known issues persist with recurring task creation:
     - Weekly/Monthly/Yearly tasks are treated as daily.
     - Duplicates are not handled correctly (see previous commit for detail)

--- src/net/sf/memoranda/test/TaskOperationsTest.java
+++ src/net/sf/memoranda/test/TaskOperationsTest.java
@@ -5,12 +5,16 @@ package net.sf.memoranda.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Vector;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import nu.xom.Attribute;
 import nu.xom.Element;
+import net.sf.memoranda.CurrentProject;
 import net.sf.memoranda.Project;
 import net.sf.memoranda.ProjectImpl;
 import net.sf.memoranda.Task;
@@ -48,20 +52,34 @@ public class TaskOperationsTest {
 
 	@Test
 	public void testRecurringTaskCreation() {
+		Calendar temp = CalendarDate.toCalendar(CalendarDate.today().getDay(), CalendarDate.today().getMonth(), CalendarDate.today().getYear());
+		CalendarDate dateDay = new CalendarDate(temp).tomorrow();
+		temp.add(temp.DATE, 7);
+		CalendarDate dateWeek = new CalendarDate(temp);
+		//TODO
+		CalendarDate dateMonth = new CalendarDate(CalendarDate.toDate(CalendarDate.today().getDay(), 
+				(CalendarDate.today().getMonth() + 1) % 12, CalendarDate.today().getYear()));
+		temp.add(temp.DATE, -7);
+		temp.add(temp.DATE, 365);
+		CalendarDate dateYear = new CalendarDate(temp);
 		Task t1 = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
-				"ext", 1, 2, "This is a description!", null, false, 10, 1, false,
-				new CalendarDate(CalendarDate.toDate(CalendarDate.today().getDay(), (CalendarDate.today().getMonth() + 2) % 12, CalendarDate.today().getYear())));	
+				"ext", 1, 2, "This is a description!", null, false, 10, 1, false, dateDay);	
 		Task t2 = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
-				"txt", 1, 2, "This is a description!", null, false, 10, 2, false,
-				new CalendarDate(CalendarDate.toDate(CalendarDate.today().getDay(), (CalendarDate.today().getMonth() + 2) % 12, CalendarDate.today().getYear())));
+				"txt", 1, 2, "This is a description!", null, false, 10, 2, false, dateWeek);
 		Task t3 = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
-				"tet", 1, 2, "This is a description!", null, false, 10, 3, false,
-				new CalendarDate(CalendarDate.toDate(CalendarDate.today().getDay(), (CalendarDate.today().getMonth() + 2) % 12, CalendarDate.today().getYear())));
+				"tet", 1, 2, "This is a description!", null, false, 10, 3, false, dateMonth);
 		Task t4 = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
-				"tex", 1, 2, "This is a description!", null, false, 10, 4, false,
-				new CalendarDate(CalendarDate.toDate(CalendarDate.today().getDay(), (CalendarDate.today().getMonth() + 2) % 12, CalendarDate.today().getYear())));
+				"tex", 1, 2, "This is a description!", null, false, 10, 4, false, dateYear);
 		// TODO Need to first determine how I'm going to make recurring tasks.
-		assertTrue(true);
+		assertTrue(t1.isRepeatable() && t2.isRepeatable() && t3.isRepeatable() && t4.isRepeatable());
+		Vector<Task> vTask = (Vector<Task>) CurrentProject.getTaskList().getRepeatableTaskforDate(dateDay);
+		//assertTrue(vTask.get(0).getText() == t1.getText());
+		vTask = (Vector<Task>) CurrentProject.getTaskList().getRepeatableTaskforDate(dateWeek);
+		//assertTrue(vTask.get(0).getText() == t2.getText());
+		vTask = (Vector<Task>) CurrentProject.getTaskList().getRepeatableTaskforDate(dateMonth);
+		//assertTrue(vTask.get(0).getText() == t3.getText());
+		vTask = (Vector<Task>) CurrentProject.getTaskList().getRepeatableTaskforDate(dateYear);
+		//assertTrue(vTask.get(0).getText() == t4.getText());
 	}
 	
 	@Test
@@ -84,9 +102,8 @@ public class TaskOperationsTest {
 	
 	@Test
 	public void testWorkingDaysOnlyRecurCreation() {
-		Task t4 = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
-				"tex", 1, 2, "This is a description!", null, false, 10, 4, true, null);
-		// TODO Need to determine how to create recurring tasks first.
-		assertTrue(true);
+		Task t = testTL.createTask(CalendarDate.today(), CalendarDate.today(),
+				"text", 1, 2, "This is a description!", null, true, 10, 4, false, null);
+		assertTrue(t.getWorkingDaysOnly());
 	}
 }
