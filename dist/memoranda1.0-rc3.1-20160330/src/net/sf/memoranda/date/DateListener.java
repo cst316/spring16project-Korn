9c0ccddc42b41bddb44a52bf80a1a6006dd88378
acmay1 <acmay1@asu.edu>
2016-04-01 10:54:28 -0700

    I just deleted the Dist's
    hopefully this will help

--- dist/memoranda1.0-rc3.1-20160330/src/net/sf/memoranda/date/DateListener.java
+++
@@ -1,8 +0,0 @@
-package net.sf.memoranda.date;
-
-/*$Id: DateListener.java,v 1.2 2004/01/30 12:17:41 alexeya Exp $*/
-public interface DateListener {
-
-  void dateChange(CalendarDate date);
-
-}
--- dist/memoranda1.0-rc3.1-20160330/src/net/sf/memoranda/ui/ExpandablePanel.java
+++
@@ -1,9 +0,0 @@
-package net.sf.memoranda.ui;
-
-import java.awt.event.ActionListener;
-
-/*$Id: ExpandablePanel.java,v 1.2 2004/01/30 12:17:41 alexeya Exp $*/
-public interface ExpandablePanel {
-
-    void AddExpandListener(ActionListener al);
-}
